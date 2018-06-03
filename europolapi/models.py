5cba0ef5289ca9c5c0af2bf2af176dff77cd7356
Teun <teun.willems+github@live.nl>
2018-06-03 15:36:56 +0200

    HashedLicensePlate can be blank

--- europolapi/models.py
+++ europolapi/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Vehicle(models.Model):
-    HashedLicensePlate = models.TextField(max_length=64, default="")
+    HashedLicensePlate = models.TextField(max_length=64, default="", blank=True)
     serialNumber = models.TextField(max_length=128, default="", primary_key=True)
     originCountry = models.TextField(max_length=64, default="")
 
