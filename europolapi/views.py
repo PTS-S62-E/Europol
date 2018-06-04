2b691e84e3e66bf1e441ff387fa9441ca5ca802b
teunw <teun.willems+github@live.nl>
2018-06-04 14:40:17 +0200

    Added search functionality

--- europolapi/views.py
+++ europolapi/views.py
@@ -1,5 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
 
 from europolapi.models import Vehicle
 from europolapi.serializers import VehicleSerializer
@@ -8,5 +9,6 @@ from europolapi.serializers import VehicleSerializer
 class VehicleViewSet(viewsets.ModelViewSet):
     queryset = Vehicle.objects.all()
     serializer_class = VehicleSerializer
-    filter_backends = (DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('HashedLicensePlate', 'serialNumber', 'originCountry')
+    search_fields = filter_fields
